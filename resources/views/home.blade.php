cdba3ee59b999fa2f1a1bddd5cab48b341e64618
upchengup <clmt0812@gmail.com>
2019-08-12 15:59:18 +0800

    修改首頁

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    {{ __('You are logged in!') }}
                 </div>
             </div>
         </div>
