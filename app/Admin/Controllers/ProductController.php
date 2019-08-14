f842eb3ff79d26d3cddcd002f7f96b888d991025
upchengup <clmt0812@gmail.com>
2019-08-14 14:18:34 +0800

    商品管理

---
+++ app/Admin/Controllers/ProductController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Product;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+
+class ProductController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'App\Product';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Product);
+
+        $grid->column('id', __('Id'));
+        $grid->column('title', __('Title'));
+        $grid->column('description', __('Description'));
+        $grid->column('image', __('Image'));
+        $grid->column('on_sale', __('On sale'));
+        $grid->column('price', __('Price'));
+        $grid->column('created_at', __('Created at'));
+        $grid->column('updated_at', __('Updated at'));
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Product::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('title', __('Title'));
+        $show->field('description', __('Description'));
+        $show->field('image', __('Image'));
+        $show->field('on_sale', __('On sale'));
+        $show->field('price', __('Price'));
+        $show->field('created_at', __('Created at'));
+        $show->field('updated_at', __('Updated at'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Product);
+
+        $form->text('title', __('Title'));
+        $form->textarea('description', __('Description'));
+        $form->image('image', __('Image'));
+        $form->switch('on_sale', __('On sale'))->default(1);
+        $form->number('price', __('Price'));
+
+        return $form;
+    }
+}
--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -12,5 +12,6 @@ Route::group([
 
     $router->get('/', 'HomeController@index')->name('admin.home');
     $router->resource('users', UserController::class);
+    $router->resource('products', ProductController::class);
 
 });
